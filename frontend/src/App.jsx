77b054a1abe044f3950fe2797cd2838b7085f104
Aakarsh Singh <singhaakarsh1506@gmail.com>
2026-09-06 16:14:34 +0530

    Updated Routing for the new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,41 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import CriminalList from "./pages/CriminalList";
 import CriminalProfile from "./pages/CriminalProfile";
+import UploadDoc from "./pages/UploadDoc";
+import CriminalListPage from "./pages/CriminalListPage";
 import "./App.css";
 
-function App() {
+// Navbar is hidden on these routes (pre-login / pre-dashboard pages)
+const NO_NAVBAR_PATHS = ["/", "/login"];
+
+function AppLayout() {
+  const location = useLocation();
+  const showNavbar = !NO_NAVBAR_PATHS.includes(location.pathname);
+
   return (
-    <BrowserRouter>
+    <>
+      {showNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/search" element={<CriminalList />} />
         <Route path="/criminal/:id" element={<CriminalProfile />} />
+        <Route path="/upload" element={<UploadDoc />} />
+        <Route path="/criminal-list" element={<CriminalListPage />} />
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppLayout />
     </BrowserRouter>
   );
 }
